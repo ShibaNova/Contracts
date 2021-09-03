56b86f746adc1bd1332b5a65f440fb1e90225e1e
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2021-09-03 06:54:59 -0400

    Revert "Add smart contract for crowdsale(PreICO, ICO)"
    This reverts commit d4d084281451043a9cd3ec8130b6dfd4634f7270.

--- shibanova/migrations/2_deploy.js
+++ shibanova/migrations/2_deploy.js
@@ -4,7 +4,6 @@ const FeeManager = artifacts.require('FeeManager')
 const ShibaBonusAggregator = artifacts.require('ShibaBonusAggregator')
 const ShibaMoneyPot = artifacts.require('ShibaMoneyPot')
 const MasterShiba = artifacts.require('MasterShiba')
-const ShibaCrowdsale = artifacts.require("ShibaCrowdsale");
 
 const swapPenaltyMaxPeriod = 84600;
 const swapPenaltyMaxPerSNova = 30;
@@ -15,17 +14,6 @@ const _devaddr =  '0x729F3cA74A55F2aB7B584340DDefC29813fb21dF';
 const _NovaPerBlock = '1000000000000000000';
 const _startBlock = 10387726;
 const _initialUpdateMoneyPotPeriodNbBlocks = 28880;
-
-const ether = (n) => new web3.BigNumber(web3.toWei(n, 'ether'));
-
-const duration = {
-  seconds: function (val) { return val; },
-  minutes: function (val) { return val * this.seconds(60); },
-  hours: function (val) { return val * this.minutes(60); },
-  days: function (val) { return val * this.hours(24); },
-  weeks: function (val) { return val * this.days(7); },
-  years: function (val) { return val * this.days(365); },
-};
 
 module.exports = async function(deployer, network, accounts) {
    
@@ -62,33 +50,4 @@ module.exports = async function(deployer, network, accounts) {
         )
 
     await _Nova.mint(accounts[0], '1000000000000000000000')
-
-    const latestTime = (new Date).getTime();
-
-    const _rate           = 500;
-    const _wallet         = accounts[0]; // TODO: Replace me
-    const _token          = _Nova.address; // can replace with _sNova
-    const _openingTime    = latestTime + duration.minutes(1);
-    const _closingTime    = _openingTime + duration.weeks(1);
-    const _cap            = ether(100);
-    const _goal           = ether(50);
-    const _foundersFund   = accounts[0]; // TODO: Replace me
-    const _foundationFund = accounts[0]; // TODO: Replace me
-    const _partnersFund   = accounts[0]; // TODO: Replace me
-    const _releaseTime    = _closingTime + duration.days(1);
-
-    await deployer.deploy(
-        ShibaCrowdsale,
-        _rate,
-        _wallet,
-        _token,
-        _cap,
-        _openingTime,
-        _closingTime,
-        _goal,
-        _foundersFund,
-        _foundationFund,
-        _partnersFund,
-        _releaseTime
-    );
 }
